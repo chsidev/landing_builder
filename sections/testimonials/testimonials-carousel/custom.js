e865879acc0971b93e4ce60b22d28e82323e0de1
chsidev <127224337+chsidev@users.noreply.github.com>
2022-09-12 12:07:30 +0700

    Refactor config loader

---
+++ sections/testimonials/testimonials-carousel/custom.js
@@ -0,0 +1,19 @@
+$('.carousel-4item-dots-marg').owlCarousel({
+    loop: false,
+    margin: 60,
+    nav: false,
+    autoplay: true,
+    autoplayHoverPause: true,
+    rewind: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+});# Change 3 on 2022-09-12
